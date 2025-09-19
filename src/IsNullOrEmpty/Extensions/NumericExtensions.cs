0a2521490b1895530de4e1ed4489383e46e719cf
Kebechet <samosidor@gmail.com>
2025-09-19 20:41:27 +0200

    added numeric extensions

---
+++ src/IsNullOrEmpty/Extensions/NumericExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace IsNullOrEmpty.Extensions
+{
+    public static class NumericExtensions
+    {
+        public static bool IsNullOrZero(this int? value) =>
+            value is null || value == 0;
+
+        public static bool IsNullOrZero(this long? value) =>
+            value is null || value == 0L;
+
+        public static bool IsNullOrZero(this double? value) =>
+            value is null || value == 0.0;
+
+        public static bool IsNullOrZero(this decimal? value) =>
+            value is null || value == 0m;
+
+        public static bool IsNullOrZero(this float? value) =>
+            value is null || value == 0f;
+    }
+}
