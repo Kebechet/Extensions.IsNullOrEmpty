940dc7d637a987948e83a10fb3a191b98381087b
Kebechet <samosidor@gmail.com>
2025-09-19 08:59:15 +0200

    added extensions

---
+++ src/IsNullOrEmpty/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IsNullOrEmpty
+{
+    public static class GuidExtensions
+    {
+    	public static bool IsNullOrEmpty(this Guid? guid)
+    	{
+    		return guid is null || guid == Guid.Empty;
+    	}
+    }
+}
