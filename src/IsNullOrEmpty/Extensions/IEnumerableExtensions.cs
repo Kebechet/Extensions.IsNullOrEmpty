3f14e91b7ca0a081f2058a26a0017f5aaacdb390
Kebechet <samosidor@gmail.com>
2026-06-20 15:25:59 +0200

    Add ICollection fast-path to IEnumerable.IsNullOrEmpty
    List<T>, arrays, dictionaries and other ICollection<T>/IReadOnlyCollection<T> sources now check Count instead of falling through to Any(), which enumerates the first element.

--- src/IsNullOrEmpty/Extensions/IEnumerableExtensions.cs
+++ src/IsNullOrEmpty/Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
@@ -6,9 +6,24 @@ namespace IsNullOrEmpty.Extensions
 {
     public static class IEnumerableExtensions
     {
-    	public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? enumerable)
-    	{
-    		return enumerable is null || !enumerable.Any();
-    	}
+        public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? enumerable)
+        {
+            if (enumerable is null)
+            {
+                return true;
+            }
+
+            if (enumerable is ICollection<T> collection)
+            {
+                return collection.Count == 0;
+            }
+
+            if (enumerable is IReadOnlyCollection<T> readOnlyCollection)
+            {
+                return readOnlyCollection.Count == 0;
+            }
+
+            return !enumerable.Any();
+        }
     }
 }
